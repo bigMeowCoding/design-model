bc83e98551b86fec4e065280ed01feb8a95de5e6
zhouyijun <zhou495248579@hotmail.com>
2021-11-28 20:18:01 +0800

    设置数组只有一个对象

--- src/pages/class.ts
+++ src/pages/class.ts
@@ -1,7 +1,15 @@
 import { RcFile } from "antd/lib/upload";
 
 export class UploadFile {
-  constructor(public name: string, public id: string) {}
+  public name: string = "";
+  public uid: string = "";
+  constructor() {}
+  getBaseIf(manage: UploadManage, id: string) {
+    console.log(id, "id=====");
+    manage.setExternalAttr(id, this);
+    console.log(this);
+    return this;
+  }
 }
 
 export class UploadManage {
@@ -13,8 +21,17 @@ export class UploadManage {
     this.cache[id] = file;
   }
   del(id: string) {
-    debugger
     this.cache[id] = null;
     delete this.cache[id];
+  }
+  setExternalAttr(id: string, uploadFile: UploadFile) {
+    const obj = this.cache[id];
+    if (!obj) {
+      return null;
+    }
+    for (const key in obj) {
+      // @ts-ignore
+      uploadFile[key] = obj[key];
+    }
   }
 }
--- src/pages/flyweight.tsx
+++ src/pages/flyweight.tsx
@@ -1,5 +1,5 @@
-import { FC, useRef, useState } from "react";
 import * as React from "react";
+import { FC, useCallback, useRef, useState } from "react";
 import { Button, Upload } from "antd";
 import { UploadOutlined } from "@ant-design/icons";
 import { UploadFile, UploadManage } from "./class";
@@ -7,6 +7,11 @@ import { UploadFile, UploadManage } from "./class";
 const FlyWeight: FC = () => {
   const [fileList, setFileList] = useState<UploadFile[]>([]);
   const manageRef = useRef(new UploadManage());
+  const uploadFileRef = useRef(new UploadFile());
+
+  const getId = useCallback((index: number) => {
+    return Object.keys(manageRef.current.cache)[index];
+  }, []);
 
   return (
     <>
@@ -16,8 +21,8 @@ const FlyWeight: FC = () => {
         beforeUpload={(file, files) => {
           const manage = manageRef.current;
           manage.add(file.uid, file);
-          const uploadFiles = files.map((item) => {
-            return new UploadFile(item.name, item.uid);
+          const uploadFiles = files.map(() => {
+            return uploadFileRef.current;
           });
           setFileList([...uploadFiles]);
           console.log("ma", manage);
@@ -26,20 +31,20 @@ const FlyWeight: FC = () => {
       >
         <Button icon={<UploadOutlined />}>Upload Directory</Button>
       </Upload>
-      {fileList.map((item) => {
+      {fileList.map((item, index) => {
         return (
-          <div key={item.id}>
-            {item.name}
+          <div key={item.getBaseIf(manageRef.current, getId(index)).uid}>
+            {item.getBaseIf(manageRef.current, getId(index)).name}
             <a
               href="#"
               onClick={() => {
                 const manage = manageRef.current;
-                manage.del(item.id);
-                const index = fileList.findIndex((findItem) => {
-                  return item.id === findItem.id;
+                manage.del(getId(index));
+                const findIndex = fileList.findIndex((findItem) => {
+                  return item.uid === findItem.uid;
                 });
-                if (index !== -1) {
-                  fileList.splice(index, 1);
+                if (findIndex !== -1) {
+                  fileList.splice(findIndex, 1);
                   setFileList([...fileList]);
                 }
                 console.log(manage);
