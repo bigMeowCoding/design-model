20e73c9aed2c391acd178653e9bbc45d78fc9202
zhouyijun <zhou495248579@hotmail.com>
2022-11-12 16:11:17 +0800

    feat:建造者模式

--- src/pages/builder/index.tsx
+++ src/pages/builder/index.tsx
@@ -1,9 +1,76 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FC } from "react";
 
 interface Props {}
 
 const BuilderMain: FC<Props> = () => {
+  class Product {
+    private parts: string[] = [];
+
+    public add(part: string) {
+      this.parts.push(part);
+    }
+
+    public show() {
+      for (const part of this.parts) {
+        console.log(part);
+      }
+    }
+  }
+
+  abstract class Builder {
+    public abstract buildPartA(): void;
+    public abstract buildPartB(): void;
+    public abstract getResult(): Product;
+  }
+
+  class ConcreteBuilderA extends Builder {
+    private product = new Product();
+    getResult(): Product {
+      return this.product;
+    }
+
+    buildPartA(): void {
+      this.product.add("部件A");
+    }
+
+    buildPartB(): void {
+      this.product.add("部件B");
+    }
+  }
+
+  class ConcreteBuilderB extends Builder {
+    private product = new Product();
+    getResult(): Product {
+      return this.product;
+    }
+
+    buildPartA(): void {
+      this.product.add("部件X");
+    }
+
+    buildPartB(): void {
+      this.product.add("部件Y");
+    }
+  }
+  class Director {
+    public Construct(br: Builder) {
+      br.buildPartA();
+      br.buildPartB();
+    }
+  }
+  useEffect(() => {
+    const director = new Director();
+    console.log("builderA---------");
+    const builderA = new ConcreteBuilderA();
+    director.Construct(builderA);
+    builderA.getResult().show();
+
+    console.log("builderB--------");
+    const builderB = new ConcreteBuilderB();
+    director.Construct(builderB);
+    builderB.getResult().show();
+  }, []);
   return <div>建造者模式</div>;
 };
 
